4718763f9b05dc9da11f4e4701c84764a36fad14
hr-sobuj <sobujhd@gmail.com>
2024-03-03 00:06:29 +0600

    routing setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import Login from "./pages/Login"
-import Registration from "./pages/Registration"
+import { useSelector } from "react-redux";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import {
+  Routes,
+  Route
+} from "react-router-dom";
+import { FC } from "react";
+import HomePage from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
-function App() {
+interface userDataType {
+  username: string,
+  token: string
+}
+
+const App: FC = () => {
+  const userData: userDataType = JSON.parse(localStorage.getItem('userData') || '');
   return (
     <>
-      {/* <Login/> */}
-      <Registration/>
+      <Routes>
+        {userData?.username ? (
+          <>
+            <Route path="/" element={<HomePage />} />
+          </>
+        ) : (
+          <>
+            <Route path="/login" element={<Login />} />
+            <Route path="/registration" element={<Registration />} />
+          </>
+        )}
+        <Route path='/' element={<HomePage />} />
+        <Route path='*' element={<NotFound />} />
+      </Routes>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import App from './App.tsx'
 import './index.css'
 import { Provider } from 'react-redux'
 import { store } from './store/store.tsx'
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </Provider>,
+  <BrowserRouter>
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>
+  </BrowserRouter>
 )
