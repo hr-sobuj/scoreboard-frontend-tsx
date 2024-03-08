617e0da870e30831f57300567c5fe076ec4d7e73
hr-sobuj <sobujhd@gmail.com>
2024-03-08 23:17:55 +0600

    some changes

--- src/utilities/axiosInterceptors.ts
+++ src/utilities/axiosInterceptors.ts
@@ -8,28 +8,28 @@ const axiosHttp = axios.create();
 axiosHttp.defaults.headers.common['Content-Type'] = 'application/json';
 axiosHttp.defaults.withCredentials = true;
 
-axiosHttp.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    console.log(error);
-    const originalUrl = error.config.url;
-    const splitUrl = originalUrl.split('/');
-    const sliceUrl = splitUrl.slice(6);
-    const actionType = sliceUrl[1];
-    const id = sliceUrl[2];
-    console.log(sliceUrl);
-    if (error.response.status === 401 || error.response.status === 500) {
-      try {
-        const result = await axiosHttp.get(refreshTokenUrl);
-        const data = await result.data;
-        actionType === 'delete' ? store.dispatch(deleteScore(id)) : store.dispatch(updateScore(id));
-      } catch (error) {
-        throw new Error(error.message);
-      }
-    }
-  }
-);
+// axiosHttp.interceptors.response.use(
+//     (response) => {
+//         return response;
+//     },
+//     async (error) => {
+//         console.log(error);
+//         const originalUrl = error.config.url;
+//         const splitUrl = originalUrl.split('/');
+//         const sliceUrl = splitUrl.slice(6);
+//         const actionType = sliceUrl[1];
+//         const id = sliceUrl[2];
+//         console.log(sliceUrl);
+//         if (error.response.status === 401 || error.response.status === 500) {
+//             try {
+//                 const result = await axiosHttp.get(refreshTokenUrl);
+//                 const data = await result.data;
+//                 actionType === 'delete' ? store.dispatch(deleteScore(id)) : store.dispatch(updateScore(id));
+//             } catch (error) {
+//                 throw new Error(error.message);
+//             }
+//         }
+//     }
+// );
 
 export default axiosHttp;
